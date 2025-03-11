d4bb05309321b458bd12f3ec768db193cdeae80b
gkwon <gkwon@student.42seoul.kr>
2025-03-11 17:41:20 +0900

    initial commit

---
+++ src/main/java/com/luckeat/luckeatbackend/LuckeatApplication.java
@@ -0,0 +1,13 @@
+package com.luckeat.luckeatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckeat/luckeatbackend/LuckeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckeat.luckeatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
