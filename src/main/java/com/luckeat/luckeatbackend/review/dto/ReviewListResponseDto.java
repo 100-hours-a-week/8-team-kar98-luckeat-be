f924eaca9fa00b848c5abd8fa4195df49683beeb
hyun.lee <qwe7105c@naver.com>
2025-03-20 14:04:55 +0900

    feat: 리뷰 API 및 DTO에 Swagger 문서화 추가

--- src/main/java/com/luckeat/luckeatbackend/review/dto/ReviewListResponseDto.java
+++ src/main/java/com/luckeat/luckeatbackend/review/dto/ReviewListResponseDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 @Builder
@@ -13,6 +14,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ReviewListResponseDto {
 
+	@Schema(description = "리뷰 목록", example = "[{...}, {...}]")
 	private List<ReviewResponseDto> reviews;
+
+	@Schema(description = "총 페이지 수", example = "5")
 	private Integer totalPages;
 }
--- src/main/java/com/luckeat/luckeatbackend/review/dto/ReviewUpdateDto.java
+++ src/main/java/com/luckeat/luckeatbackend/review/dto/ReviewUpdateDto.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
 @Setter
@@ -15,11 +16,14 @@ public class ReviewUpdateDto {
     @NotNull(message = "평점은 필수입니다")
     @Min(value = 1, message = "평점은 1~5 사이여야 합니다")
     @Max(value = 5, message = "평점은 1~5 사이여야 합니다")
+    @Schema(description = "평점 (1~5)", example = "4")
     private Integer rating;
     
     @NotBlank(message = "리뷰 내용은 필수입니다")
     @Size(min = 5, message = "리뷰 내용은 5글자 이상이어야 합니다")
+    @Schema(description = "리뷰 내용", example = "이 가게는 정말 좋았습니다!")
     private String reviewContent;
     
+    @Schema(description = "리뷰 이미지 URL", example = "http://example.com/review.jpg")
     private String reviewImage;
 }
