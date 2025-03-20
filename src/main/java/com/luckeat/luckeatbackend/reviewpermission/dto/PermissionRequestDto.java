6fc08f2c7c5fd93ed204179d5b2ac12cf686f07e
hyun.lee <qwe7105c@naver.com>
2025-03-20 14:17:44 +0900

    feat: 리뷰 권한 API 및 DTO에 Swagger 문서화 추가

--- src/main/java/com/luckeat/luckeatbackend/reviewpermission/dto/PermissionRequestDto.java
+++ src/main/java/com/luckeat/luckeatbackend/reviewpermission/dto/PermissionRequestDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 @Builder
@@ -13,8 +14,10 @@ import lombok.NoArgsConstructor;
 public class PermissionRequestDto {
 
 	@NotNull(message = "스토어 ID는 필수입니다")
+	@Schema(description = "스토어 ID", example = "123")
 	private Long storeId;
 	
 	@NotNull(message = "권한을 부여할 사용자 ID는 필수입니다")
+	@Schema(description = "권한을 부여할 사용자 ID", example = "456")
 	private Long targetUserId;
 }
